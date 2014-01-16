a7a39aa1babbe95f6a332c22dc5d777b442b5076
Kyle DeWeese <ocean1888@gmail.com>
2014-01-16 01:22:11 +0000

    Adding first version.

---
+++ show.js
@@ -0,0 +1,22 @@
+var image1=new Image()
+image1.src="image1.jpg"
+var image2=new Image()
+image2.src="image2.jpg"
+var image3=new Image()
+image3.src="image3.jpg"
+var image4=new Image()
+image4.src="image4.jpg"
+
+var step=1
+function slideit()
+{
+if(!document.images)
+return
+document.images.slide.src=eval("image"+step+".src")
+if (step < 4)
+step++
+else
+step=1
+setTimeout("slideit()", 9000)
+}
+slideit()
